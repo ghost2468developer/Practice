f603a77835aa62899275494d6a0b345bca7468f9
OutlawDev <siyabonga.ncube669@gmail.com>
2024-03-15 16:41:08 +0200

    dsfdsf

--- TypeScript/server/src/resolvers.ts
+++ TypeScript/server/src/resolvers.ts
@@ -1,6 +1,23 @@
+interface User {
+  id: string
+  name: string
+  email: string
+}
+
+let users: User[] = []
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello, World!'
+    hello: () => 'Hello, World!',
+    users: () => users,
+    user: (parent: any, args: { id: string }) => users.find(user => user.id === args.id)
+  },
+  Mutation: {
+    createUser: (parent: any, args: { name: string, email: string }) => {
+      const newUser = { id: String(users.length + 1), ...args }
+      users.push(newUser);
+      return newUser;
+    }
   }
 }
 
--- TypeScript/server/src/schema.ts
+++ TypeScript/server/src/schema.ts
@@ -1,8 +1,20 @@
 import { gql } from 'apollo-server-express'
 
 const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
   type Query {
     hello: String
+    users: [User]
+    user(id: ID!): User
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User
   }
 `
 
